ca0185ccd5320d4cfa04397747b3d0e82883ae1c
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-10 20:46:42 +0530

    * install vault * run vault login * get secrets from vault

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 vault login $VAULT_TOKEN
-vault kv get $SECRET_NAME |  sed -n '/= Data =/,$ p' | grep -Ev '= Data =|^Key|^--' | awk '{ print "export " $1"="$2}'
+vault kv get $SECRET_NAME |  sed -n '/= Data =/,$ p' | grep -Ev '= Data =|^Key|^--' | awk '{ print "export " $1"="$2}' >/data/secrets
