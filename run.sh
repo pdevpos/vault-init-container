d3c2eb159f90176baa08d2932c8cccac6a8d4bf1
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-10 21:13:42 +0530

    remove path

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 vault login $VAULT_TOKEN
-vault kv get $SECRET_NAME |  sed -n '/= Data =/,$ p' | grep -Ev '= Data =|^Key|^--' | awk '{ print "export " $1"="$2}' >/data/secrets
-cat /data/secrets
+vault kv get $SECRET_NAME |  sed -n '/= Data =/,$ p' | grep -Ev '= Data =|^Key|^--' | awk '{ print "export " $1"="$2}'
