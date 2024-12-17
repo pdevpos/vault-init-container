9a96fdee67544dd2cad3a3bb6e285f67be722cc6
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-17 17:28:39 +0530

    mak a directory

--- practice1/run.sh
+++ practice1/run.sh
@@ -1,6 +1,6 @@
 mkdir /test
 vault kv get $SECRET_NAME |  sed -n '/= Data =/,$ p' | grep -Ev '= Data =|^Key|^--' | awk '{ print "export "$1"=\""$2"\""}' >/test/secrets.txt
-
+cat /test/secrets.txt
 
 
 
